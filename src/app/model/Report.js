f842869cf68fa2ac4c80bcaf7d4e311d67a37514
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-01-17 17:55:58 -0800

    feat: define the basic actions a report class can do

--- src/app/model/Report.js
+++ src/app/model/Report.js
@@ -1,3 +1,7 @@
+import { FirebaseDatabase } from "../shared/firebase/firebaseDatabase";
+
+const REPORT_PATH = "/reports";
+
 export class Report{
 
     /**@type {string} */
@@ -35,5 +39,38 @@ export class Report{
         this.dateEntry = reportType.dateEntry;
         this.typeID = reportType.typeID;
         this.companyID = reportType.companyID
+    }
+
+     /**
+      * 
+      * @param {object} query 
+      * @param {string} query.reporterID
+      * @param {string} query.reportedID
+      * @param {string} query.companyID
+      * @param {string} query.typeID
+      * @param {EpochTimeStamp} query.dateEntry
+      * @param {string} query.companyID
+      */
+    static async reportThisUser(query){
+        const report = new Report(query)
+        await FirebaseDatabase.pushDataToDB({
+            newData:report,
+            queryPath:REPORT_PATH,
+        })
+        return report
+    }
+
+    /**
+     * 
+     * @param {string} userID 
+     * @returns {[Report]}
+     */
+    static async getReportedUserByUserID(userID){
+      const result = await FirebaseDatabase.readDataFromDByEquality({
+        equalValue:userID,
+        queryKey:'reportedID',
+        queryPath:REPORT_PATH
+      }) 
+      return Object.values(result)
     }
 }
