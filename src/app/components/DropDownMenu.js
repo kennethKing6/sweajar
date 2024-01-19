eb0c6460e296648b4458452761eb3d4a71582949
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-01-18 17:57:24 -0800

    Update DropDownMenu.js

--- src/app/components/DropDownMenu.js
+++ src/app/components/DropDownMenu.js
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+//import { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+//import { Menu, MenuItem} from '@mui/material';
 
 function DropdownMenu() {
   // Fetch the menu items from the database and store them in an array
-  const [menuItems, setMenuItems] = useState([]);
+  //const [menuItems, setMenuItems] = useState([]);
 
-  useEffect( () => {
+  /*useEffect( () => {
     fetch("")
       .then( (response) => response.json )
       .then( (data) => setMenuItems(data) );
-  }, []);
+  }, []);*/
 
   // Store the selected menu item in a state variable
-  const [selectedItem, setSelectedItem] = useState("");
+  //const [selectedItem, setSelectedItem] = useState("");
 
   const handleSelect = (eventKey) => {
     setSelectedItem(eventKey);
   };
 
   return (
-    <Dropdown onSelect={handleSelect}>
+    <Dropdown /*onSelect={handleSelect}*/>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
-        {selectedItem || "Select a violation"}
+        Select a violation
       </Dropdown.Toggle>
   
       <Dropdown.Menu>
-        {menuItems.map( (item) => (
+        {/* {menuItems.map( (item) => (
           <Dropdown.Item key={item.id} eventKey={item.name}>
             {item.name}
           </Dropdown.Item>
-        ))}
+        ))} */}
+        <Dropdown.Item>Profanity</Dropdown.Item>
+        <Dropdown.Item>Late Arrival</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
