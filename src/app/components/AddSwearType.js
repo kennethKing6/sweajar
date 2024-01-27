fb637a5bccb82994c5dbcd4ff79a630305f36149
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-01-26 23:11:01 -0800

    Update AddSwearType.js

--- src/app/components/AddSwearType.js
+++ src/app/components/AddSwearType.js
@@ -1,3 +1,24 @@
+import { SwearType } from "../model/SwearType";
+
 export default function AddSwearType ({onAdd = () => {}}) {
     const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const onSubmit = async () => {
+        // Validate the input fields
+        if (!name || !description) {
+            alert ("Please enter a name a description for the new swear type.");
+            return;
+        }
+        // Create a new swear type object
+        const query = {name, description};
+        const newSwearType = await SwearType.createNewSwearType(query);
+        onAdd(newSwearType);
+    };
+
+    return (
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+            
+        </Box>
+    );
 }
