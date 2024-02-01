3f47fe7494b7c01fc2bf6fbf8176039e335a4cb1
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-01-31 17:52:45 -0800

    feat: firebase auth base class

---
+++ src/app/shared/firebase/firebaseAuth.js
@@ -0,0 +1,55 @@
+import { FirebaseConfigs } from "./FirebaseConfig";
+import {
+    GoogleAuthProvider,
+    signInWithRedirect,
+    signInAnonymously,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    User
+    
+} from 'firebase/auth'
+
+const auth = FirebaseConfigs.firebaseAuth
+let userAuthStateUnsubscribe;
+
+export class FirebaseAuth{
+
+    static async signUserAnonymously(){
+        return await signInAnonymously(auth)
+    }
+    static async signInUser(email,password){
+      return await signInWithEmailAndPassword(auth,email,password)
+    }
+
+    static async signUserUp(email,password){
+       return await createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    static async signOutUser(){
+      return await signOut(auth)
+    }
+
+
+    /**
+     * 
+     * @callback userSignedInCallback
+     * @returns {User|null} 
+     * 
+     */
+    /**
+     * 
+     * @param {userSignedInCallback} callback 
+     */
+    static async listenForUserAuthState(callback){
+        userAuthStateUnsubscribe =  onAuthStateChanged(auth,(user)=>{
+            callback(user)
+        })
+    }
+
+    static unsubscribeUserStateListener (){
+        if(userAuthStateUnsubscribe)
+            userAuthStateUnsubscribe()
+    }
+}
