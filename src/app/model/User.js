38d314583ce95c4b0e52ff9d80b3598c26678a07
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-01-31 17:53:25 -0800

    feat: update user actions

--- src/app/model/User.js
+++ src/app/model/User.js
@@ -1,15 +1,13 @@
+import { FirebaseAuth } from "../shared/firebase/firebaseAuth";
 import { FirebaseDatabase } from "../shared/firebase/firebaseDatabase";
+import { SignedInUser } from "./SignedInUser";
 
 export class User {
 
-    /**@type {string} */
-    username = '';
 
     /**@type {string} */
     profilePicture;
 
-     /**@type {string} */
-     employeedID = '';
 
     /**@type {string} */
     firstName = '';
@@ -18,78 +16,147 @@ export class User {
      lastName = '';
 
     /**@type {string} */
-    companyID = '';
+    teamID = '';
 
     /**@type {string} */
     userID = '';
 
+    /**@type {string} */
+    email = '';
+
     /**
      * 
      * @param {object} user 
-     * @param {string} user.username
      * @param {string} user.profilePicture
-     * @param {string} user.employeedID
      * @param {string} user.firstName
      * @param {string} user.lastName
-     * @param {string} user.companyID
+     * @param {string} user.teamID
      * @param {string} user.userID
+     * @param {string} user.email
      */
     constructor(user){
-        this.companyID = user.companyID;
-        this.employeedID = user.employeedID
-        this.firstName = user.firstName;
-        this.lastName = user.lastName;
-        this.profilePicture = user.profilePicture || "https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-        this.username = user.username;
-        this.userID = user.userID
+        this.teamID = user.teamID || ""
+        this.firstName = user.firstName 
+        this.lastName = user.lastName 
+        this.userID = user.userID 
+        this.email = user.email
+        try{
+            this.profilePicture = user.profilePicture || "https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+        }catch(err){}
     }
     /**
      * 
      * @param {object} query 
-     * @param {string} query.username
      * @param {string} query.profilePicture
-     * @param {string} query.employeedID
-     * @param {string} query.companyID
+     * @param {string} query.teamID
      * @param {string} query.firstName
      * @param {string} query.lastName
-     * @param {string} query.username
      * @param {string} query.userID
+     * @param {string} query.email
+     * @param {string} query.password
      */
-    static async createUser(query){
-        const user = new User({
-            companyID:query.companyID,
-            employeedID:query.employeedID,
+    static async createAccount(query){
+        let user = new User({
+            teamID:query.teamID,
             firstName:query.firstName,
             lastName:query.lastName,
             profilePicture:query.profilePicture,
-            username:query.username
+            email:query.email,
+            userID:""
         })
+        const createdUserCredential = await FirebaseAuth.signUserUp(query.email,query.password)
+        user = new User({
+            teamID:query.teamID,
+            firstName:query.firstName,
+            lastName:query.lastName,
+            profilePicture:query.profilePicture,
+            email:query.email,
+            userID:createdUserCredential.user.uid
+        })
+        await FirebaseAuth.signOutUser()
+
        await FirebaseDatabase.writeDataToDB({
            queryPath:`/users/${user.userID}`,
            data:user
         })
     }
 
-    static async getUsersByCompanyID(userID,companyID){
-       var result = await FirebaseDatabase.readDataFromDByEquality({
-        equalValue:userID,
-        queryKey: '/userID',
-        queryPath:'/users'
-       }) 
-       result = Object.values(result)[0]
-       //check if user requesting is saved under that company
-       if(!result)throw new Error("Unauthorized")
+    static async signIn(email,password){
 
-       const user = new User(result)
+      const userCredentials = await FirebaseAuth.signInUser(email,password);
+     const result =  await FirebaseDatabase.readDataFromDB({
+        queryPath:`/users/${userCredentials.user.uid}`,
+      })
 
-       if(user.companyID !== companyID)throw new Error("UnAuthorized")
+      SignedInUser.user = new User(result)
 
-       const users = await FirebaseDatabase.readDataFromDByEquality({
-        equalValue:companyID,
-        queryKey:'companyID',
-        queryPath:'/users'
-       })
-       return Object.values(users)
+    }
 
+    static async signOut(){
+        await FirebaseAuth.signOutUser()
+        SignedInUser.user = null;
+    }
+
+    static async updateCurrentTeam(teamID){
+        if(!SignedInUser.user) await FirebaseAuth.signOutUser()
+
+        await FirebaseDatabase.updateDataOnDB({
+            newData:teamID,
+            queryPath:`/users/${SignedInUser.user.userID}/teamID`
+        })
+    }
+
+    static async getUsersByteamID(userID,teamID){
+        if(userID && teamID){
+            var result = await FirebaseDatabase.readDataFromDByEquality({
+                equalValue:userID,
+                queryKey: '/userID',
+                queryPath:'/users'
+               }) 
+               result = Object.values(result)[0]
+               //check if user requesting is saved under that company
+               if(!result)throw new Error("Unauthorized")
+        
+               const user = new User(result)
+        
+               if(user.teamID !== teamID)throw new Error("UnAuthorized")
+        
+               const users = await FirebaseDatabase.readDataFromDByEquality({
+                equalValue:teamID,
+                queryKey:'teamID',
+                queryPath:'/users'
+               })
+               return Object.values(users)
+        }
+       
+        return []
+    }
+
+    /**
+     * 
+     * @param {string} userID 
+     */
+    static async getUserByID(userID){
+        return await FirebaseDatabase.readDataFromDB({
+            queryPath:`/users/${userID}`
+        })
+    }
+
+    static listenForUserState(callback){
+        FirebaseAuth.listenForUserAuthState(async (user)=>{
+            if(user){
+                const result =  await FirebaseDatabase.readDataFromDB({
+                    queryPath:`/users/${user.uid}`
+                  })
+                if(result){
+                    SignedInUser.user = new User(result)
+                    callback(user)
+                }else{
+                    callback(null)
+                }
+            }else{
+                callback(null)
+            }
+        })
     }
 }
