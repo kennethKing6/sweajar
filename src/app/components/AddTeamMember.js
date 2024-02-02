120feed05b5c62577152c6269b3666c01baf2e40
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-02-01 20:33:44 -0800

    Create AddTeamMember.js

---
+++ src/app/components/AddTeamMember.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+import {
+    List,
+    ListItem,
+    ListSubheader,
+    TextField,
+    Button,
+} from "@mui/material";
+import { Teams } from "../model/Teams";
+
+export default function AddTeamMember ({onAdd = () => {}}) {
+}
