186ca70f3c01c09fb29b2e11130c198279289a57
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-02-01 19:22:45 -0800

    Create TeamViewer.js

---
+++ src/app/components/TeamViewer.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState,useEffect } from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Checkbox,
+  Divider,
+} from "@mui/material";
+import { Teams } from "../model/Teams";
+import AddSwearType from "./AddSwearType";
+
+export default function TeamViewer() {
+    const [selected, setSelected] = useState([]);
+    const [items, setItems] = useState([]);
+    const onToggle = (item) => {
+        const index = selected.indexOf(item);
+        if (index > -1) {
+            // Remove the item from the array
+            setSelected(selected.filter((i) => i !== item));
+        } else {
+            // Add the item to the array
+            setSelected(item);
+        }
+    };
+
+    useEffect(() => {
+        // Fetch the list items from the database
+        const fetchItems = async () => {
+            const teams = await Teams.getTeams();
+            alert(JSON.stringify(teams))
+            setItems(teams);
+        };
+        fetchItems().then().catch()
+    }, []);
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={6} lg={8} sx={{ position: 'relative', alignSelf: 'flex-start', justifyContent: 'flex-end', alignItems: 'flex-start' }}>
+                <Box sx={{ 
+                    width: '100%', 
+                    bgcolor: 'black', 
+                    color: 'white', 
+                    padding: 2,
+                    border: '2px solid yellow'
+                }}>
+                    <h1>
+                    Team Viewer
+                    <SortButton onPress={sortAlphabetically} />
+                    </h1>
+                    <List>
+                        {items.map( (item) => (
+                            <ListItem onClick={() => {
+                                onToggle(item.name)
+                                SwearType.selectReport({
+                                    name:item.name
+                                })
+                            }}>
+                                <ListItemIcon>
+                                    <Checkbox checked={selected.includes(item.teamName)} />
+                                </ListItemIcon>
+                                <ListItemText primary={item.teamName} sx={{color:'black'}}/>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Grid>
+        </Grid>
+    );
+}
