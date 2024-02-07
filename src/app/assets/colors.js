1911d04f5475b687a3dcf04227b153118704ad84
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-02-06 20:22:00 -0800

    feat: colors definition

---
+++ src/app/assets/colors.js
@@ -0,0 +1,19 @@
+const PRIMARY_COLOR = "black";
+const SECONDARY_COLOR = "yellow";
+const ACCENT_COLOR_1 = "white";
+const ACCENT_COLOR_2 = "#FFC107";
+const ACCENT_COLOR_3 = "#FF5733";
+const ACCENT_COLOR_4 = "#FFEB3B"
+const ACCENT_COLOR_5 = "#F2BB13"
+
+
+
+export const Colors = {
+    PRIMARY_COLOR,
+    SECONDARY_COLOR,
+    ACCENT_COLOR_1,
+    ACCENT_COLOR_2,
+    ACCENT_COLOR_3,
+    ACCENT_COLOR_4,
+    ACCENT_COLOR_5
+}
---
+++ src/app/assets/sizes.js
@@ -0,0 +1,4 @@
+
+export const TEXT_SIZES = {}
+export const MARGIN_SIZES = {}
+export const PADDING_SIZES = {}
