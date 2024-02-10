74b10245d97c644a567ded1382eefe12320282b8
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-02-09 19:05:17 -0800

    feat: define the basic styles available resources

--- src/app/assets/fonts.js
+++ src/app/assets/fonts.js
@@ -1,6 +1,15 @@
 
 
 
+export const FontSizes = {
+    titleFontSize:34, // Title font size
+    largeFontSize:20, // Large font size
+    bodyFontSize:17, // Body font size
+    captionFontSize:15,  // Caption font size
+    smallFontSize: 13 // Small font size
+}
+
+
 export const Fonts = {
-    
+   
 }
