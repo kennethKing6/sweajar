96295e8b33ad4815b46406622106dc7c0f8a5a5c
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-02-15 18:52:01 -0800

    Update ViolationSelectList.js
    icon buttons now swap different components

--- src/app/components/ViolationSelectList.js
+++ src/app/components/ViolationSelectList.js
@@ -9,7 +9,6 @@ import {
   ListSubheader,
   Checkbox,
   Tooltip,
-  Divider,
   IconButton,
 } from "@mui/material";
 import { SwearType } from "../model/SwearType";
@@ -17,6 +16,7 @@ import { SignedInUser } from "../model/SignedInUser";
 import ReportButton from "./ReportButton";
 import AddSwearType from "./AddSwearType";
 import Button from "./Button";
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 export default function ViolationSelectList({
@@ -25,6 +25,7 @@ export default function ViolationSelectList({
 }) {
   const [selected, setSelected] = useState([]);
   const [items, setItems] = useState([]);
+  const [showViolations, setShowViolations] = useState(true);
   const [showAddSwearType, setShowAddSwearType] = useState(false);
   const onToggle = (item) => {
     const index = selected.indexOf(item);
@@ -62,112 +63,116 @@ export default function ViolationSelectList({
         <h1>
           New Report
         </h1>
+        <Tooltip title="Show Violations" placement="top">
+          <IconButton onClick={() => {setShowViolations(true), setShowAddSwearType(false)}}>
+            <FormatListBulletedIcon sx={{ backgroundColor: 'yellow', color: 'black' }}/>
+          </IconButton>
+        </Tooltip>
         <Tooltip title="Add a New Swear Type" placement="top">
-          <IconButton onClick={() => setShowAddSwearType(!showAddSwearType)}>
+          <IconButton onClick={() => {setShowViolations(false), setShowAddSwearType(true)}}>
             <PlaylistAddIcon sx={{ backgroundColor: 'yellow', color: 'black' }}/>
           </IconButton>
         </Tooltip>
       </Box>
-      <List subheader={
-        <ListSubheader component="div" id="list-subheader" sx={{color: "white", bgcolor: "black"}}>
-          Select the Violation
-        </ListSubheader>
-      }>
-        <Tooltip title="Choose this option if the user used inappropriate language" placement="right">
-          <ListItem onClick={() => {
-            onToggle("Profanity")
-            addDefaultViolation({
-              name:"Profanity",
-              description:"Choose this option if the user used inappropriate language"
-            })
-          }}>
-            <ListItemIcon>
-              <Checkbox checked={selected.includes("Profanity")} sx={{color:'white'}}/>
-            </ListItemIcon>
-            <ListItemText sx={{color:'white'}}>
-              Profanity
-            </ListItemText>
-          </ListItem>
-        </Tooltip>
-
-        <Tooltip title="Choose this option if the user's microphone was muted" placement="right">
-          <ListItem onClick={() =>{ 
-            onToggle("Muted Microphone")
-            addDefaultViolation({
-              name:"Muted Microphone",
-              description:"Choose this option if the user's microphone was muted"
-            })
-            }}>
-            <ListItemIcon>
-              <Checkbox checked={selected.includes("Muted Microphone")} sx={{color:'white'}}/>
-            </ListItemIcon>
-            <ListItemText sx={{color:'white'}}>
-              Muted Microphone
-            </ListItemText>
-          </ListItem>
-        </Tooltip>
-
-        <Tooltip title="Choose this option if the user joined the meeting late" placement="right">
-          <ListItem onClick={() => {
-            onToggle("Late Arrival")
-            addDefaultViolation({
-              name:"Late Arrival",
-              description:"Choose this option if the user joined the meeting late"
-            })
-            }}>
-            <ListItemIcon>
-              <Checkbox checked={selected.includes("Late Arrival")} sx={{color:'white'}}/>
-            </ListItemIcon>
-            <ListItemText sx={{color:'white'}}>
-              Late Arrival
-            </ListItemText>
-          </ListItem>
-        </Tooltip>
-
-        {items.map( (item) => (
-          <Tooltip title={item.description} placement="right" key={item.id}>
+      {showViolations && (
+        <List subheader={
+          <ListSubheader component="div" id="list-subheader" sx={{color: "white", bgcolor: "black"}}>
+            Select the Violation
+          </ListSubheader>
+        }>
+          <Tooltip title="Choose this option if the user used inappropriate language" placement="right">
             <ListItem onClick={() => {
-              onToggle(item.name)
-              SwearType.selectReport({
-                description:item.description,
-                levels:item.level,
-                name:item.name
+              onToggle("Profanity")
+              addDefaultViolation({
+                name:"Profanity",
+                description:"Choose this option if the user used inappropriate language"
               })
             }}>
               <ListItemIcon>
-                <Checkbox checked={selected.includes(item.name)} sx={{color:'white'}}/>
+                <Checkbox checked={selected.includes("Profanity")} sx={{color:'white'}}/>
               </ListItemIcon>
-              <ListItemText primary={item.name} sx={{color:'white'}}/>
+              <ListItemText sx={{color:'white'}}>
+                Profanity
+              </ListItemText>
             </ListItem>
           </Tooltip>
-        ))}
-        <ListItem>
-            <Button text="Next"
-              onPress={()=>{
-                if(!SwearType.hasSwearTypes()){
-                  alert("Please select a violation");
-                  return
-                }
-                if(!SignedInUser.user.teamID){
-                  alert("Please select a team")
-                  return
-                }
-                onNavigateToUserToReport()
-              }}
-              sx={{ 
-                backgroundColor: '#FFEB3B', 
-                color: 'black', 
-                '&:hover':{
-                    backgroundColor: '#FFC107',
-                }
-              }}
-            />
-        </ListItem>
-        <br></br>
-        <Divider color='white'/>
-        <br></br>
-        {showAddSwearType && (<AddSwearType />)}
-      </List>
+
+          <Tooltip title="Choose this option if the user's microphone was muted" placement="right">
+            <ListItem onClick={() =>{ 
+              onToggle("Muted Microphone")
+              addDefaultViolation({
+                name:"Muted Microphone",
+                description:"Choose this option if the user's microphone was muted"
+              })
+              }}>
+              <ListItemIcon>
+                <Checkbox checked={selected.includes("Muted Microphone")} sx={{color:'white'}}/>
+              </ListItemIcon>
+              <ListItemText sx={{color:'white'}}>
+                Muted Microphone
+              </ListItemText>
+            </ListItem>
+          </Tooltip>
+
+          <Tooltip title="Choose this option if the user joined the meeting late" placement="right">
+            <ListItem onClick={() => {
+              onToggle("Late Arrival")
+              addDefaultViolation({
+                name:"Late Arrival",
+                description:"Choose this option if the user joined the meeting late"
+              })
+              }}>
+              <ListItemIcon>
+                <Checkbox checked={selected.includes("Late Arrival")} sx={{color:'white'}}/>
+              </ListItemIcon>
+              <ListItemText sx={{color:'white'}}>
+                Late Arrival
+              </ListItemText>
+            </ListItem>
+          </Tooltip>
+
+          {items.map( (item) => (
+            <Tooltip title={item.description} placement="right" key={item.id}>
+              <ListItem onClick={() => {
+                onToggle(item.name)
+                SwearType.selectReport({
+                  description:item.description,
+                  levels:item.level,
+                  name:item.name
+                })
+              }}>
+                <ListItemIcon>
+                  <Checkbox checked={selected.includes(item.name)} sx={{color:'white'}}/>
+                </ListItemIcon>
+                <ListItemText primary={item.name} sx={{color:'white'}}/>
+              </ListItem>
+            </Tooltip>
+          ))}
+          <ListItem>
+              <Button text="Next"
+                onPress={()=>{
+                  if(!SwearType.hasSwearTypes()){
+                    alert("Please select a violation");
+                    return
+                  }
+                  if(!SignedInUser.user.teamID){
+                    alert("Please select a team")
+                    return
+                  }
+                  onNavigateToUserToReport()
+                }}
+                sx={{ 
+                  backgroundColor: '#FFEB3B', 
+                  color: 'black', 
+                  '&:hover':{
+                      backgroundColor: '#FFC107',
+                  }
+                }}
+              />
+          </ListItem>
+        </List>
+      )}
+      {showAddSwearType && (<AddSwearType />)}
     </Box>
   );
 }
