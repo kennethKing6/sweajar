0e1253b72400a723d7295a83847da81d34944da4
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-03-02 13:11:16 -0800

    test is now working properly

--- src/components/CreateNewTeam.test.js
+++ src/components/CreateNewTeam.test.js
@@ -1,10 +1,24 @@
-import CreateNewTeam from "../components/CreateNewTeam";
-import { Teams } from "../model/Teams";
+import "../model/__mocks__/SwearType";
+import "../shared/firebase/__mock__/mockFirebase";
+import "../model/__mocks__/User";
 import { render, fireEvent } from "@testing-library/react";
 import React from "react";
+import { User } from "../model/User";
+import { SignedInUser } from "../model/SignedInUser";
+import { Teams } from "../model/Teams";
+import CreateNewTeam from "../components/CreateNewTeam";
 
-jest.mock('../model/Teams', () => ({
-    createTeam: jest.fn(),
+jest.mock('../model/Teams');
+
+// SignedInUser.user = {
+//     userID:"userID"
+// }
+jest.mock('../model/AppState', () => ({
+    user: { userID: "userID" }
+}));
+
+jest.mock('../model/SignedInUser', () => ({
+    user: { userID: "userID" }
 }));
 
 describe('CreateNewTeam', () => {
@@ -12,6 +26,7 @@ describe('CreateNewTeam', () => {
 
     beforeEach(() => {
         mockOnAdd = jest.fn();
+        Teams.createTeam.mockClear();
     });
 
     it('renders without crashing', () => {
@@ -19,14 +34,18 @@ describe('CreateNewTeam', () => {
     });
 
     it('calls Teams.createTeam when form is submitted', async () => {
-        const { getByLabelText, getByText } = render(<CreateNewTeam/>);
+        const { getByLabelText, getByText } = render(<CreateNewTeam onAdd={mockOnAdd}/>);
         const teamNameInput = getByLabelText('Team Name');
         const createButton = getByText('Create');
+        const teamName = 'Test Team';
 
-        fireEvent.change(teamNameInput, { target: { value: 'Test Team'}});
+        Teams.createTeam.mockResolvedValue({ id: '1', name: teamName });
+
+        fireEvent.change(teamNameInput, { target: { value: teamName }});
         fireEvent.click(createButton);
 
-        expect(Teams.createTeam).toHaveBeenCalledWith('Test Team');
+        const teams = await Teams.createTeam("name");
+        await expect(Teams.createTeam).toHaveBeenCalledWith(teamName);
     });
 
     it('does not call Teams.createTeam when team name is empty', () => {
