b0095b6ded1a32995fd81dc163f18a36a6f18842
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-03-30 06:04:26 -0700

    added LeaderboardListItem test

---
+++ src/components/LeaderboardListItem.test.js
@@ -0,0 +1,47 @@
+import "../shared/firebase/__mock__/mockFirebase"
+import "../model/__mocks__/User";
+import React from 'react';
+import { render } from '@testing-library/react';
+import LeaderboardListItem from './LeaderboardListItem';
+
+describe('LeaderboardListItem', () => {
+    it('renders without crashing', () => {
+        render(<LeaderboardListItem />);
+    });
+
+    // it('renders list items correctly', () => {
+    //     const { getAllByRole } = render(<LeaderboardListItem />);
+    //     const listItems = getAllByRole('listitem');
+    //     expect(listItems.length).toBeGreaterThan(0);
+    // });
+
+    it('renders profile pictures', () => {
+        const { getAllByRole } = render(<LeaderboardListItem />);
+        const profilePictures = getAllByRole('img');
+        expect(profilePictures.length).toBeGreaterThan(0);
+    });
+
+    it('renders primary text correctly', () => {
+        const { getByText } = render(<LeaderboardListItem />);
+        const primaryText = getByText(/Birthday Gift Test/);
+        expect(primaryText).toBeInTheDocument();
+    });
+
+    it('renders secondary text correctly', () => {
+        const { getByText } = render(<LeaderboardListItem />);
+        const secondaryText = getByText(/I'll be in the neighbourhood/);
+        expect(secondaryText).toBeInTheDocument();
+    });
+
+    it('renders chips correctly', () => {
+        const { getAllByRole } = render(<LeaderboardListItem />);
+        const chips = getAllByRole('button', { name: /Do you have a suggestion/ });
+        expect(chips.length).toBeGreaterThan(0);
+    });
+
+    it('renders secondary action chips correctly', () => {
+        const { getAllByRole } = render(<LeaderboardListItem />);
+        const secondaryActionChips = getAllByRole('button', { label: '30' });
+        expect(secondaryActionChips.length).toBeGreaterThan(0);
+    });
+});
