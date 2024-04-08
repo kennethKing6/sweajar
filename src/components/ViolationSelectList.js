99d09e0e0dd2ce42542a6750e35ef1ccc2c10f24
sin1465 <156178275+sin1465@users.noreply.github.com>
2024-04-07 21:18:17 -0700

    removed BubbleUI

--- src/components/ViolationSelectList.js
+++ src/components/ViolationSelectList.js
@@ -48,7 +48,6 @@ import { NewReportReduxActions } from "../shared/redux/actions/newReportActions"
 import { TextFieldStyles } from "../assets/TextFieldStyles";
 import { ButtonStyles } from "../assets/ButtonStyles";
 import { Padding_Sizes } from "../assets/paddingSizes";
-import { BubbleListComponent } from "./BubbleUI";
 function ViolationSelectList({
   onPress = () => {},
   onNavigateToUserToReport = () => {},
