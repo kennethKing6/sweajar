7ff037590fff383f2c73d5732d6c32b4a211d4fb
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2024-09-14 12:06:12 -0700

    Update UserDetailsChart.js

--- src/components/UserDetailsChart.js
+++ src/components/UserDetailsChart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
+import { Colors } from "../assets/colors";
 
 const chartSetting = {
   margin: { bottom: 20, left: 25, right: 5 },
